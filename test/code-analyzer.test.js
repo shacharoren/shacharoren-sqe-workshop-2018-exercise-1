77d7377ecfb0367348635056177c17a6a3f2082c
shacharoren <shacharorennn@gmail.com>
2018-11-25 23:54:09 +0200

    A comment for a new commit

--- test/code-analyzer.test.js
+++ test/code-analyzer.test.js
@@ -15,14 +15,6 @@ describe('The javascript parser', () => {
             '{"type":"Program","body":[],"sourceType":"script","loc":{"start":{"line":0,"column":0},"end":{"line":0,"column":0}}}'
         );
     });
-
-    it.skip('is parsing a simple variable declaration correctly', () => {
-        assert.equal(
-            JSON.stringify(parseCode('let a = 1;')),
-            '{"type": "Program","body": [{"type":"VariableDeclaration","declarations":[{"type":"VariableDeclarator","id":{"type":"Identifier","name":"a","loc":{"start":{"line":1,"column":4},"end":{"line":1,"column":5}}},"init":{"type":"Literal","value":1,"raw":"1","loc":{"start":{"line":1,"column": 8},"end":{"line":1,"column":9}}},"loc":{"start":{"line":1,"column":4},"end":{"line":1,"column":9}}}],"kind":"let","loc":{"start":{"line":1,"column":0},"end":{"line":1,"column":10}}}],"sourceType":"script","loc":{"start":{"line":1,"column":0},"end":{"line":1,"column":10}}}'
-        );
-    });
-
 });
 
 describe('The javascript parser', () => {
